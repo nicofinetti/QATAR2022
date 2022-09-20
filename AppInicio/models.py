36d01778506b250b198e539d8961862c105fa851
Maximiliano <maxibarraza97@gmail.com>
2022-09-19 23:44:26 -0300

    modelos actualizados

--- AppInicio/models.py
+++ AppInicio/models.py
@@ -6,4 +6,10 @@ class Pais(models.Model):
 
 class Jugador(models.Model):
     nombre=models.CharField(max_length=60)
-    
+    apellido=models.CharField(max_length=60)
+    pais=models.CharField(max_length=50)
+    posicion=models.CharField(max_length=25)
+
+class Estadio(models.Model):
+    localizacion=models.Charfield(max_length=30)
+
