9f5580b09c03ef7e4599aef4af12a754e500decc
Maximiliano <maxibarraza97@gmail.com>
2022-09-25 20:17:45 -0300

    Remigrado y creacion admin mxbar

--- AppInicio/models.py
+++ AppInicio/models.py
@@ -1,13 +1,10 @@
 from django.db import models
-
-# Create your models here.
 class Pais(models.Model):
     grupo=models.CharField(max_length=1)
     instanciaplayoff=models.CharField(max_length=25)
     jugadores=models.CharField(max_length=25)
 class Jugador(models.Model):
     nombre=models.CharField(max_length=60)
-    apellido=models.CharField(max_length=60)
     equipo=models.CharField(max_length=50)
     posicion=models.CharField(max_length=25)
 
@@ -17,7 +14,7 @@ class Estadio(models.Model):
 class FechaGrupos(models.Model):
     equipolocal=models.CharField(max_length=25)
     equipovisita=models.CharField(max_length=25)
-    fecha=models.DateField()
+    fecha=models.CharField(max_length=25)
     resultado=models.CharField(max_length=25)
 
 class FechaPlayOff(models.Model):
