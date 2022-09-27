00bff67235242f87608fde57e065c94338dd5f4f
RodNicolas <sonnic42@gmail.com>
2022-09-26 22:38:43 -0300

    Revert "Update models.py"
    This reverts commit 44e6c2556350f5fa1c2ba2a514cb29fc496d6371.

--- AppInicio/models.py
+++ AppInicio/models.py
@@ -1,13 +1,9 @@
-from email.policy import default
 from django.db import models
-
 
 # Create your models here.
 class Pais(models.Model):
-    pais = models.CharField(max_length=60)
+    pais=models.CharField(max_length=60)
 
 class Jugador(models.Model):
-    nombre = models.CharField(max_length=60)
-    
-
+    nombre=models.CharField(max_length=60)
     
