e244298e2add027112fcb5a4161442016cf638f2
RodNicolas <sonnic42@gmail.com>
2022-09-28 01:33:13 -0300

    Update settings.py

--- QATAR2022/settings.py
+++ QATAR2022/settings.py
@@ -38,7 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'AppInicio',
+<<<<<<< HEAD
     'ForoNews',
+=======
+    
+    
+>>>>>>> rodnicolas
 ]
 
 MIDDLEWARE = [
